97b41098617856a2f45b21641256a4596fb9ce15
Alonso <asanmor011004@gmail.com>
2024-11-14 18:23:40 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FootballAdminProject_AlonsoSanchez',
+  webDir: 'www'
+};
+
+export default config;
