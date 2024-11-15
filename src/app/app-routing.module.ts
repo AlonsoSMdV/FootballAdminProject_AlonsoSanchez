e29457b6158fa09540303ffe1771511e550ca20b
AlonsoSMdV <alonsosmdv@gmail.com>
2024-11-15 14:32:20 +0100

    Created more files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,31 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'home',
+    loadChildren: () => import('./page/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'leagues',
+    loadChildren: () => import('./page/leagues/leagues.module').then( m => m.LeaguesPageModule)
+  },
+  {
+    path: 'teams',
+    loadChildren: () => import('./page/teams/teams.module').then( m => m.TeamsPageModule)
+  },
+  {
+    path: 'players',
+    loadChildren: () => import('./page/players/players.module').then( m => m.PlayersPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./page/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./page/register/register.module').then( m => m.RegisterPageModule)
   }
+
 ];
 
 @NgModule({
